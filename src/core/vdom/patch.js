bde181f5af8fa9468555887740a4185885388f4f
Andrew Chung <achung89@users.noreply.github.com>
2023-08-07 16:24:20 -0400

    fix(patch): patch update algorithm for dynamic css scopes

--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -40,7 +40,8 @@ function sameVnode (a, b) {
         a.tag === b.tag &&
         a.isComment === b.isComment &&
         isDef(a.data) === isDef(b.data) &&
-        sameInputType(a, b)
+        sameInputType(a, b) &&
+        findScopeId(a) === findScopeId(b)
       ) || (
         isTrue(a.isAsyncPlaceholder) &&
         isUndef(b.asyncFactory.error)
@@ -55,6 +56,20 @@ function sameInputType (a, b) {
   const typeA = isDef(i = a.data) && isDef(i = i.attrs) && i.type
   const typeB = isDef(i = b.data) && isDef(i = i.attrs) && i.type
   return typeA === typeB || isTextInputType(typeA) && isTextInputType(typeB)
+}
+
+function findScopeId (node) {
+  if (isDef(node.fnScopeId)) {
+    return node.fnScopeId
+  }
+
+  let ancestor = node
+  while (ancestor) {
+    if (isDef(ancestor.context) && isDef(ancestor.context.$options._scopeId)) {
+      return ancestor.context.$options._scopeId
+    }
+    ancestor = ancestor.parent
+  }
 }
 
 function createKeyToOldIdx (children, beginIdx, endIdx) {
